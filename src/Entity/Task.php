6625b081a865bdfe062252dcb6aad965acbcc636
Mouhamed Hatoum <administrateur@MacBook-Pro-de-Administrateur.local>
2021-04-15 10:43:52 +0200

    er

---
+++ src/Entity/Task.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $completeAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeInterface
+    {
+        return $this->createDate;
+    }
+
+    public function setCreateDate(\DateTimeInterface $createDate): self
+    {
+        $this->createDate = $createDate;
+
+        return $this;
+    }
+
+    public function getCompleteAt(): ?\DateTimeInterface
+    {
+        return $this->completeAt;
+    }
+
+    public function setCompleteAt(?\DateTimeInterface $completeAt): self
+    {
+        $this->completeAt = $completeAt;
+
+        return $this;
+    }
+}
